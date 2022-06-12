0189957d96dabfbccae3500497e0ebda277176f8
Mucahit Acikgoz <acikgozmucahit91@gmail.com>
2022-06-12 20:02:17 +0200

    test branchs

--- src/test/java/automationexcercise/tests/TestClass01.java
+++ src/test/java/automationexcercise/tests/TestClass01.java
@@ -1,2 +1,14 @@
-package automationexcercise.tests;public class TestClass01 {
+package automationexcercise.tests;
+
+import automationexcercise.utilities.ConfigReader;
+import automationexcercise.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class TestClass01 {
+
+    @Test
+    public void test01(){
+        Driver.getDriver().get(ConfigReader.getProperty("base_url"));
+        Driver.getDriver().close();
+    }
 }
