e97d614b2255c6d7fe15dffb4c38323837f98094
AcikgozMucahit <85254260+AcikgozMucahit@users.noreply.github.com>
2022-06-16 17:45:15 +0200

    Update HomePage.java

--- src/test/java/automationexcercise/pages/HomePage.java
+++ src/test/java/automationexcercise/pages/HomePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
+    
     public HomePage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
