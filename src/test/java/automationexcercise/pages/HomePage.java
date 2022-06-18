08e898ed33d42fbdb36e2ce0ad3ad19af1da7765
AcikgozMucahit <85254260+AcikgozMucahit@users.noreply.github.com>
2022-06-18 17:20:37 +0200

    Create HomePage.java

---
+++ src/test/java/automationexcercise/pages/HomePage.java
@@ -0,0 +1,24 @@
+package automationexcercise.pages;
+
+import automationexcercise.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+
+    public HomePage(){
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy(xpath = "//a[@href='/login']")
+    public WebElement signUpButton;
+    @FindBy(xpath = "(//h2)[3]")
+    public WebElement newUserSignUpText;
+    @FindBy(xpath = "//input[@name='name']")
+    public WebElement nameBox;
+    @FindBy(xpath = "//*[text()='Bestami Colak']")
+    public WebElement loggedInAs;
+    @FindBy(xpath = "//a[@href='/delete_account']")
+    public WebElement deleteAccountButton;
+}
