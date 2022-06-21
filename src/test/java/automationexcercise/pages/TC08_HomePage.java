8120e30eb6bac09384725aa308a91da3bb6640d8
Bilal Ergin <bilalergin1587@gmail.com>
2022-06-21 19:22:21 +0300

    update bilal

--- src/test/java/automationexcercise/pages/TC08_HomePage.java
+++ src/test/java/automationexcercise/pages/TC08_HomePage.java
@@ -1,4 +1,5 @@
 package automationexcercise.pages;
+
 import automationexcercise.utilities.Driver;
 import org.apache.xmlbeans.impl.xb.xsdschema.FieldDocument;
 import org.openqa.selenium.WebElement;
@@ -8,15 +9,16 @@ import org.openqa.selenium.support.PageFactory;
 
 public class TC08_HomePage {
 
-    public TC08_HomePage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+    public TC08_HomePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
+
     @FindBy(xpath = "(//a[@href='/'])[2]")
     public WebElement homePage;
     @FindBy(xpath = "//a[@href = '/products']")
     public WebElement productsBtn;
-    @FindBy (xpath = "//h2//h2")
-    public WebElement ALLPRODUCTtext;
+    @FindBy(xpath = "//h2[text()='All Products']")
+    public WebElement allProductText;
     @FindBy(xpath = "//a[@href = '/product_details/1']")
     public WebElement viewProduct;
 }
--- src/test/java/automationexcercise/tests/TC08_Test.java
+++ src/test/java/automationexcercise/tests/TC08_Test.java
@@ -10,17 +10,21 @@ import org.testng.annotations.Test;
 public class TC08_Test {
 
     TC08_HomePage tc08_page = new TC08_HomePage();
+
     @Test
-    public void tc08_test() {
+    public void tc08_test() throws InterruptedException {
 //1. Launch browser
 //2. Navigate to url 'http://automationexercise.com'
         Driver.getDriver().get(ConfigReader.getProperty("base_url"));
 //3. Verify that home page is visible successfully
         tc08_page.homePage.click();
 //4. Click on 'Products' button
-//5. Verify user is navigated to ALL PRODUCTS page successfully
-// 6. The products list is visible
         tc08_page.productsBtn.click();
+//5. Verify user is navigated to ALL PRODUCTS page successfully,
+
+        Assert.assertTrue(tc08_page.allProductText.isDisplayed());
+        Thread.sleep(5000);
+// 6. The products list is visible
 //7. Click on 'View Product' of first product
 //8. User is landed to product detail page
 //9. Verify that detail detail is visible: product name, category, price, availability, condition, brand
@@ -28,11 +32,4 @@ public class TC08_Test {
     }
 
 
-
-
-
-
-
-
-
-    }
+}
