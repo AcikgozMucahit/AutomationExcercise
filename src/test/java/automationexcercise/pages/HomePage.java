db9d27c27bf6fa4b9e46f9b0a4d25480044e2833
sefikkesim <sefikkesim@gmail.com>
2022-06-21 13:36:11 +0300

    Test Case 10

--- src/test/java/automationexcercise/pages/HomePage.java
+++ src/test/java/automationexcercise/pages/HomePage.java
@@ -1,9 +1,27 @@
 package automationexcercise.pages;
 
+import automationexcercise.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 
     public HomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
     }
+    @FindBy(id = "footer")
+    public WebElement footer;
+
+    @FindBy(xpath = "//div[@class = 'single-widget']/h2")
+    public WebElement subscriptionText;
+
+    @FindBy(id = "susbscribe_email")
+    public WebElement subscriptionEmail;
+
+    @FindBy(id = "subscribe")
+    public WebElement subscriptionSubmitBtn;
+
+    @FindBy(xpath = "//div[@class = 'alert-success alert']")
+    public WebElement subscriptionSuccessAlert;
 }
---
+++ src/test/java/automationexcercise/tests/TestCase10.java
@@ -0,0 +1,51 @@
+package automationexcercise.tests;
+
+import automationexcercise.pages.HomePage;
+import automationexcercise.utilities.ConfigReader;
+import automationexcercise.utilities.Driver;
+import automationexcercise.utilities.JSUtils;
+import automationexcercise.utilities.ReusableMethods;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class TestCase10 {
+    HomePage homePage;
+    @Test
+    public void testCase09() {
+        homePage = new HomePage();
+
+        Driver.getDriver().get(ConfigReader.getProperty("automationPracticeWebSite"));
+        String title = Driver.getDriver().getTitle();
+        String url = Driver.getDriver().getCurrentUrl();
+
+        SoftAssert softAssert = new SoftAssert();
+        Assert.assertEquals(title, ConfigReader.getProperty("title"), "The title did not match");
+        Assert.assertEquals(url, ConfigReader.getProperty("url"), "The url did not match");
+
+        softAssert.assertEquals(title, ConfigReader.getProperty("title"), "The title did not match");
+        softAssert.assertEquals(url, ConfigReader.getProperty("url"), "The url did not match");
+
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        String isPageCompleted = js.executeScript("return document.readyState").toString();
+
+        softAssert.assertEquals(isPageCompleted, "complete");
+        Assert.assertEquals(isPageCompleted, "complete");
+
+        JSUtils.scrollIntoViewJS(homePage.footer);
+
+        Assert.assertTrue(homePage.subscriptionText.isDisplayed());
+
+        homePage.subscriptionEmail.sendKeys(ConfigReader.getProperty("email"));
+        homePage.subscriptionSubmitBtn.click();
+
+        ReusableMethods.waitForVisibility(homePage.subscriptionSuccessAlert,3);
+
+        Assert.assertTrue(homePage.subscriptionSuccessAlert.isDisplayed());
+
+        Driver.closeDriver();
+
+
+    }
+}
